b7b4fd1c9cee9546624e23c2b06bdd202f835bf0
vaibhavikatti <vaibhavikatti19@gmail.com>
2023-06-14 12:37:20 +0530

    completed context

--- src/component/Comments.jsx
+++ src/component/Comments.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../Context'
+
 
 function Comments() {
+  const context = useContext(DataContext)
+  const [comments] = context.commentApi.comments
   return (
-    <div>
-      comments
+   <div className="container">
+    <div className="row">
+      <div className="col-md-12 text-center">
+        <h5 className="text-success display-3">Comments</h5>
+      </div>
     </div>
+    <div className="row">
+      {
+        comments && comments.map((item,index)=>{
+          return(
+            <div className="col-md-4 mt-2 mb-2" key={index}>
+              <div className="card">
+                <div className="card-header">
+                  <h5 className="text-dark">{item.id} : {item.name}</h5>
+                </div>
+                <div className="card-body">
+                  <ul className="list-group">
+                  <li className="list-group-item">
+                    <h3 className="text-success"><strong>Email : </strong> {item.email} </h3>
+                    </li>
+                    <li className="list-group-item">
+                    <h3 className="text-warning"><strong>Comment : </strong> {item.body} </h3>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+   </div>
   )
 }
 
--- src/component/Posts.jsx
+++ src/component/Posts.jsx
@@ -9,7 +9,7 @@ function Posts() {
     <div className="container">
     <div className="row">
       <div className="col-md-12 text-center">
-        <h3 className="display-3 text-success">Users</h3>
+        <h3 className="display-3 text-success">Posts</h3>
       </div>
     </div>
     <div className="row">
@@ -18,8 +18,14 @@ function Posts() {
           return(
             <div className="col-md-4 mt-2 mb-2" key={index}>
               <div className="card">
-                <div className="card-body">
-                  <h3 className="text-info">{item.body}</h3>
+                <div className="card-header bg-info">
+                  <h5 className="text-white "> {item.id} : {item.title}</h5>
+                </div>
+                <div className="card-body bg-info">
+                  <ul className="list-group">
+                    <li className="list-group-item"><h3 className="text-info">{item.body}</h3></li>
+                  </ul>
+                  
                 </div>
               </div>
             </div>
